5096ac9e1aef9cdca8a3f9dacc6d5c28602ad917
Artem Shanin <mefistoggc@mail.ru>
2022-08-27 14:25:53 +0300

    TM-2 Добавил главную страницу для вывода списка дел

---
+++ src/views/pages/main/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Список дел</h2>
+    <p>Список дел пуст</p>
+    <div>
+      <input type="text" placeholder="Например, новая задача">
+      <button>Добавить</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage'
+}
+</script>
